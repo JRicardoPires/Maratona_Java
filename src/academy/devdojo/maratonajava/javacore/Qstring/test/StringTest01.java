92d9d0c712d006bc5b0cec0fa34e20ed8e3c1afe
João Ricardo Pires <jrpires.web@gmail.com>
2021-08-18 11:58:06 -0300

    Aula_109

--- src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest01.java
@@ -2,12 +2,12 @@ package academy.devdojo.maratonajava.javacore.Qstring.test;
 
 public class StringTest01 {
     public static void main(String[] args) {
-        String nome = "Joao"; //String constant pool
-        String nome2 = "Joao";
-        nome = nome.concat(" Ricardo");
+        String nome = "William"; //String constant pool
+        String nome2 = "William";
+        nome = nome.concat(" Suane"); // nome += " Suane"
         System.out.println(nome);
         System.out.println(nome == nome2);
-        String nome3 = new String("Joao"); // 1 variavel de referência, 2 objeto do tipo string, 3 uma string no pool de string
+        String nome3 = new String("William"); // 1 variavel de referência, 2 objeto do tipo string, 3 uma string no pool de string
         System.out.println(nome2 == nome3);
         System.out.println(nome2 == nome3.intern());
     }
